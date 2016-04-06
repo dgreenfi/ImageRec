81f6e9fd5c9b830332f5fe12c2e62369b74cd69a
ERIN MCMAHON <emcmahon013@gmail.com>
2016-04-06 01:44:05 -0400

    remove dups

--- groups.py
+++ groups.py
@@ -2,7 +2,14 @@ import pandas as pd
 import numpy as np 
 
 
-def splitter(value,empty_type):
+class cluster_eval:
+	def __init__(self,data):
+		self.data = data
+		self.dups = self.remove_dups()
+
+
+
+def splitter(self,value,empty_type):
 	if type(value) == str:
 		new_value = eval(value.replace("'",""))
 	elif type(value) == float:
@@ -10,8 +17,26 @@ def splitter(value,empty_type):
 	assert type(new_value) == type(empty_type)
 	return new_value
 
+def remove_dups(self):
+	dups = {}
+	n = len(self.data)
+	for d in self.data.index:
+		salesRank = self.data['salesRank'][d]
+		try:
+			dups[salesRank].append(d)
+		except KeyError:
+			dups[salesRank] = [d]
+	self.dups = {k:v for k,v in dups.items() if len(v) > 1}
 
-def calc_each(data,sub):
+# def clean_data(self):
+# 	n = len(self.data)
+# 	for i in range(n):
+		
+
+
+			
+
+def calc_each(self,sub):
 	n = len(data)
 	total_in = 0
 	total_both = 0
@@ -19,19 +44,19 @@ def calc_each(data,sub):
 	in_zero = 0
 	sub_mat = {}
 	for i in range(n):
-		shoe = data.iloc[i]
-		viewed = splitter(shoe[sub],list())
+		shoe = self.data.iloc[i]
+		viewed = self.splitter(shoe[sub],list())
 		in_data = 0
 		in_both = 0
 		out_data = 0
 		sub_mat[shoe.name] = []
 		for v in viewed: 
 			try: 
-				data.loc[v]
+				self.data.loc[v]
 				in_data += 1
 				sub_mat[shoe.name].append(v)
 				try:
-					if shoe.name in data.loc[v][sub]:
+					if shoe.name in self.data.loc[v][sub]:
 						in_both +=1
 				except TypeError:
 					pass
@@ -48,11 +73,11 @@ def calc_each(data,sub):
 	assert len(sub_mat) == n - in_zero
 	return sub_mat
 
-def eval_cluster(clusters,sub_mat):
-	same_cluster = 0
-	dif_cluster = 0 
+def metrics(clusters,sub_mat):
 	results = {}
 	n = len(clusters)
+	same_cluster_all = 0
+	diff_cluster_all = 0
 	for c in range(n):
 		same_cluster = 0
 		dif_cluster = 0 
@@ -64,7 +89,7 @@ def eval_cluster(clusters,sub_mat):
 						same_cluster += 1
 						same_cluster_all += 1
 					else:
-						dif_cluster + =1 
+						dif_cluster += 1 
 						dif_cluster_all += 1
 			except KeyError:
 				pass
@@ -85,7 +110,9 @@ def eval_cluster(clusters,sub_mat):
 
 if __name__ == "__main__":
 	data = pd.read_csv('./data/boots_aws_metadata.csv',index_col='asin')
-	del data['#']
-	sub_mat = calc_each(data,'also_viewed')
+	#del data['#']
+	CE = cluster_eval(data)
+	sub_mat = CE.calc_each('also_viewed')
+	print(sub_mat)
 
 
