59a010f9d9a154aae5f1ee51dc340f51a2a75ed2
ERIN MCMAHON <erinmcmahon@ERINs-MBP-2.home>
2016-04-11 00:37:41 -0400

    added bandits + distance

--- clustering.py
+++ clustering.py
@@ -2,6 +2,7 @@ import sklearn
 from pandas import *
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.mixture import VBGMM, DPGMM
 from sklearn import cluster
 from sklearn import mixture
 import csv
@@ -30,26 +31,33 @@ def output_clusters(clusters,fname):
     for item in clusters:
         file.write(','.join(item)+'\n')
 
-def main():
+def main(method,cluster_num=None,alpha=None):
     f ='/Users/davidgreenfield/Downloads/features_csv_tmp.csv'
     #f ='/Users/davidgreenfield/Downloads/features_f500.csv'
     cols=range(1,4096)
     feats =np.loadtxt(open(f,"rb"),delimiter=",",skiprows=1,usecols=(cols))
     asins = np.loadtxt(open(f,"rb"),delimiter=",",skiprows=1,usecols=([0]),dtype=str)
-    cluster_num=25
-    k_means=cluster.KMeans(n_clusters=cluster_num)
-    k_means.fit(feats)
+    if method == 'kmeans':
+        k_means=cluster.KMeans(n_clusters=cluster_num)
+        k_means.fit(feats)
+        y = kmeans.labels_
+    elif method == 'GMM_VB':
+        gmm_vb = VBGMM.fit(feats,n_components=50,alpha=alpha)
+        y = gmm_vb.predict(feats)
+        cluster_no = len(np.unique(y))
+    elif method == 'GMM_DP':
+        gmm_dp = DPGMM(n_components=50,alpha=alpha)
+        gmm_dp.fit(feats)
+        y = gmm_dp.predict(feats)
+        cluster_no = len(np.unique(y))
+
+
     clusters=[]
-
-
-
     groups={}
-
-
     data=load_data('./data/boots_aws.csv')
 
     for i in range(0,cluster_num):
-        groups[i]=np.where(k_means.labels_==i)
+        groups[i]=np.where(y==i)
         ids=asins[groups[i]]
         clusters.append(ids)
         links=[data[x]['url'] for x in ids]
